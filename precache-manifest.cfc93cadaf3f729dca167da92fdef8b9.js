077db4938ddd022ead9b8468fd3188354de6aa14
Richard Dalton <richard@devjoy.com>
2022-01-28 08:21:12 +0000

    Updates

---
+++ precache-manifest.cfc93cadaf3f729dca167da92fdef8b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d51b01829ee636973eb9d23186d544",
+    "url": "/reactTutorial/index.html"
+  },
+  {
+    "revision": "ca5acaa2d6206fb8641e",
+    "url": "/reactTutorial/static/css/main.8a935685.chunk.css"
+  },
+  {
+    "revision": "b0e6da509c92bab60928",
+    "url": "/reactTutorial/static/js/2.7cf3fb22.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/reactTutorial/static/js/2.7cf3fb22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5acaa2d6206fb8641e",
+    "url": "/reactTutorial/static/js/main.064be3dd.chunk.js"
+  },
+  {
+    "revision": "57a4fc828926467c3203",
+    "url": "/reactTutorial/static/js/runtime-main.1e9c5ad6.js"
+  }
+]);
